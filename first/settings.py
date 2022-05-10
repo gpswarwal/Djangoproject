0203c70d2588af6f9906a214f63f921cf1c8c5b5
gpswarwal <99944302+gpswarwal@users.noreply.github.com>
2022-05-10 14:25:57 +0530

    Update settings.py

--- first/settings.py
+++ first/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'first.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['html'],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
