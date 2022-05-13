06f6121312ee3a6e790d286245d5129bc869d920
gpswarwal <99944302+gpswarwal@users.noreply.github.com>
2022-05-13 10:41:41 +0530

    Update preinstall.sh

--- preinstall.sh
+++ preinstall.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo apt update
 sudo apt install python3-pip python3-dev nginx -y
-sudo pip3 install virtualenv
+sudo pip install virtualenv
 virtualenv env
 source env/bin/activate
 pip install django gunicorn
